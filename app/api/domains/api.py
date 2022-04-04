77ebd7e62bdaf9226aacfeb1b79e2b9778b34c49
def750 <def75023@gmail.com>
2022-04-04 16:52:15 +0000

    Add more data to api

--- app/api/domains/api.py
+++ app/api/domains/api.py
@@ -575,10 +575,10 @@ async def api_get_map_scores(
     # NOTE: userid will eventually become player_id,
     # along with everywhere else in the codebase.
     query = [
-        "SELECT s.map_md5, s.score, s.pp, s.acc, s.max_combo, s.mods, "
+        "SELECT s.id scoreid, s.map_md5, s.score, s.pp, s.acc, s.max_combo, s.mods, "
         "s.n300, s.n100, s.n50, s.nmiss, s.ngeki, s.nkatu, s.grade, s.status, "
         "s.mode, s.play_time, s.time_elapsed, s.userid, s.perfect, "
-        "u.name player_name, "
+        "u.name player_name, u.country, "
         "c.id clan_id, c.name clan_name, c.tag clan_tag "
         "FROM scores s "
         "INNER JOIN users u ON u.id = s.userid "
