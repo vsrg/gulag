1f3fae743bdc5be4dc4fedb168f40b7642bd4bac
def750 <def75023@gmail.com>
2022-04-08 20:35:33 +0000

    Remove zenith stage one

--- app/constants/privileges.py
+++ app/constants/privileges.py
@@ -16,25 +16,25 @@ class Privileges(IntFlag):
     """Server side user privileges."""
 
     # privileges intended for all normal players.
-    NORMAL = 1 << 0  # is an unbanned player.
-    VERIFIED = 1 << 1  # has logged in to the server in-game.
+    NORMAL = 1 << 0  # is an unbanned player.                   # 1
+    VERIFIED = 1 << 1  # has logged in to the server in-game.   # 2
 
     # has bypass to low-ceiling anticheat measures (trusted).
-    WHITELISTED = 1 << 2
+    WHITELISTED = 1 << 2 # trusted player.                      # 4
 
     # donation tiers, receives some extra benefits.
-    SUPPORTER = 1 << 4
-    PREMIUM = 1 << 5
+    SUPPORTER = 1 << 4                                          # 8
+    PREMIUM = 1 << 5                                            # 16
 
     # notable users, receives some extra benefits.
-    ALUMNI = 1 << 7
+    ALUMNI = 1 << 7                                             # 128
 
     # staff permissions, able to manage server app.state.
-    TOURNAMENT = 1 << 10  # able to manage match state without host.
-    NOMINATOR = 1 << 11  # able to manage maps ranked status.
-    MODERATOR = 1 << 12  # able to manage users (level 1).
-    ADMINISTRATOR = 1 << 13  # able to manage users (level 2).
-    DEVELOPER = 1 << 14  # able to manage full server app.state.
+    TOURNAMENT = 1 << 10  # able to manage match state without host.  # 1024
+    NOMINATOR = 1 << 11  # able to manage maps ranked status.         # 2048
+    MODERATOR = 1 << 12  # able to manage users (level 1).            # 4096
+    ADMINISTRATOR = 1 << 13  # able to manage users (level 2).        # 8192
+    DEVELOPER = 1 << 14  # able to manage full server app.state.      # 16384
 
     #* Anti Privileges
     BLOCK_AVATAR = 1 << 24
